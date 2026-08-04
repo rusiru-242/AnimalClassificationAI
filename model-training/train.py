84697cd3cd905763c6f648789cb339a1c0876ad2
rusiru-242 <rusiruasinsa100@gmail.com>
2026-08-04 21:13:30 +0530

    Build MobileNetV2 classification model

--- model-training/train.py
+++ model-training/train.py
@@ -1,15 +1,73 @@
-from utils.dataset_loader import load_datasets
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
 
+from utils.dataset_loader import load_datasets
+from config import *
+
+# Load Dataset
 train_dataset, validation_dataset, class_names = load_datasets()
 
-print("\nClasses")
+print("Classes:", class_names)
 
-print(class_names)
+# Normalize Images (0-255 -> 0-1)
+normalization_layer = layers.Rescaling(1./255)
 
-print("\nTraining Dataset")
+train_dataset = train_dataset.map(
+    lambda x, y: (normalization_layer(x), y)
+)
 
-print(train_dataset)
+validation_dataset = validation_dataset.map(
+    lambda x, y: (normalization_layer(x), y)
+)
 
-print("\nValidation Dataset")
+data_augmentation = tf.keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.15),
+    layers.RandomZoom(0.15),
+    layers.RandomContrast(0.15),
+])
 
-print(validation_dataset)
+base_model = MobileNetV2(
+    input_shape=(224,224,3),
+    include_top=False,
+    weights="imagenet"
+)
+
+base_model.trainable = False
+
+model = models.Sequential([
+
+    data_augmentation,
+
+    base_model,
+
+    layers.GlobalAveragePooling2D(),
+
+    layers.Dropout(0.3),
+
+    layers.Dense(128, activation="relu"),
+
+    layers.Dropout(0.2),
+
+    layers.Dense(len(class_names), activation="softmax")
+
+])
+
+model.compile(
+
+    optimizer="adam",
+
+    loss="sparse_categorical_crossentropy",
+
+    metrics=["accuracy"]
+
+)
+
+model.summary()
+
+tf.keras.utils.plot_model(
+    model,
+    show_shapes=True,
+    to_file="outputs/model.png"
+)
